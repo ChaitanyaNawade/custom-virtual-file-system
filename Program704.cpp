56c3bcc63f8ee674a689f8c5fbf24f58bc7c6185
ChaitanyaNawade <chaitanyanawde0@gmail.com>
2025-11-22 10:55:24 +0530

    Initialize core VFS structures

---
+++ Program704.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+// #include<unistd.h>
+#include<string.h>
+
+
+using namespace std;
+int main()
+{
+    cout<<"Jay ganesh";
+    return 0;
+}
